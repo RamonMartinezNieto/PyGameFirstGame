3df4d948ed4d42a2ca6b60dee647ff77b6adc81a
Ramón Martínez Nieto <ramon.martinez.nieto@gmail.com>
2021-02-20 17:46:27 +0100

    Add monster movement and multiple monster

--- Monster.py
+++ Monster.py
@@ -8,7 +8,7 @@ class Monster:
         self.list_monsters = []
 
         self.monster_speed = 5
-        self.monster_scale = 2
+        self.monster_scale = 4
 
         self.Config = configurationClass 
         self.surface = self.Config.GetMonsterSurface()
@@ -17,11 +17,17 @@ class Monster:
         self.direction_right = True
         self.color_monster = (0,255,0,255)
 
-        self.create_monster()
+        self.rows_monsters = 4
+        self.quanty_per_rows = 6
 
-    def create_monster(self): 
+        self.create_monster(self.quanty_per_rows,self.rows_monsters)
+
+    def create_monster(self,quantityPerRow,rowsMonster): 
         self._draw_basic_monster(10,10)
-        self.list_monsters.append([10,10])
+
+        for i in range(quantityPerRow):
+            for a in range(rowsMonster):
+                self.list_monsters.append([10*((i*self.monster_scale)*1.5),10*((a*self.monster_scale)*1.1)])
     
     def repaint_monsters(self): 
         for i in range(len(self.list_monsters)):
@@ -58,34 +64,49 @@ class Monster:
         pygame.draw.rect(self.surface, self.color_monster, (x+(s*3), y+(s*7), s*2,s))
         pygame.draw.rect(self.surface, self.color_monster, (x+(s*6), y+(s*7), s*2,s))
 
-
-
-
-        
-
+    
     def monster_movement(self): 
-
         for i in range(len(self.list_monsters)):
-
-            self._monster_movement_right(i)
-            self._monster_movement_left(i)
+            control = False
+            if self.direction_right:
+                control = self._monster_movement_right()
+            else: 
+                control = self._monster_movement_left()
+            
+            if control:
+                break
 
         
-    def _monster_movement_right(self, positionMonsterList):
-            if self.list_monsters[positionMonsterList][0] <= self.width_screen - 100 and self.direction_right: 
-                self.list_monsters[positionMonsterList][0] += self.monster_speed
+    def _monster_movement_right(self):
+        if self.list_monsters[(self.quanty_per_rows*self.rows_monsters)-1][0] <= self.width_screen - 100 and self.direction_right:      
+            self._move_all_monsters_right()
+            return True            
+        elif self.list_monsters[(self.quanty_per_rows*self.rows_monsters)-1][0] >= self.width_screen - 100 and self.direction_right:
+            self._move_all_monsters_down()
+            self.direction_right = False 
+            return True
 
-            elif self.list_monsters[positionMonsterList][0] >= self.width_screen - 100 and self.direction_right:
-                self.list_monsters[positionMonsterList][1] += self.monster_speed
-                self.direction_right = False 
-
-    def _monster_movement_left(self,positionMonsterList):
-        if self.list_monsters[positionMonsterList][0] > 50 and not self.direction_right: 
-            self.list_monsters[positionMonsterList][0] -= self.monster_speed
-
-        elif not self.direction_right and self.list_monsters[positionMonsterList][0] >= 0: 
-            self.list_monsters[positionMonsterList][1] += self.monster_speed
+    def _monster_movement_left(self):
+        if self.list_monsters[0][0] > 50 and not self.direction_right: 
+            self._move_all_monsters_left()
+            return True
+        elif not self.direction_right and self.list_monsters[0][0] >= 0: 
+            self._move_all_monsters_down()
             self.direction_right = True
+            return True
+
+
+    def _move_all_monsters_right(self):
+        for i in range(len(self.list_monsters)):
+            self.list_monsters[i][0] += self.monster_speed
+
+    def _move_all_monsters_down(self):
+        for i in range(len(self.list_monsters)):
+            self.list_monsters[i][1] += self.monster_speed
+
+    def _move_all_monsters_left(self): 
+        for i in range(len(self.list_monsters)):
+            self.list_monsters[i][0] -= self.monster_speed
 
 
     def get_list_monsters(self): 
